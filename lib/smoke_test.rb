dfa3299a46b7a5c77687e4bdf102192eda659491
Andrew Potter <hello@drewpotter.com>
2018-02-22 15:34:53 +0000

    Major reworking of the code

--- lib/smoke_test.rb
+++ lib/smoke_test.rb
@@ -5,7 +5,7 @@ require 'openssl'
 class SmokeTest
   def initialize
     secrets         = Rails.application.secrets
-    @urls           = secrets.smoke_test[:urls]
+    @urls           = secrets.smoke_test[:urls] || []
     @http_header    = secrets.smoke_test[:http_header]
     @api_token      = secrets.smoke_test[:api_token] || ""
     @slack_uri      = secrets.smoke_test[:slack_uri]
@@ -16,23 +16,22 @@ class SmokeTest
   end
 
   def test_endpoints
-    message = []
-    message << "Smoke Test results"
-    if @urls.nil?
-      message << "Urls are not properly configured"
-      slack_smoke_notification message.join("\n")
-      return
+    messages = []
+
+    if @urls.blank?
+      messages << "Please set the urls as an array in the Rails secrets or see the documentation."
     end
 
     @urls.each do |url|
-      message << test_url(url)
+      messages << test_url(url)
     end
 
-    slack_smoke_notification message.join("\n")
+    slack_smoke_notification(messages)
   end
 
   def test_url(url)
-    parse_result(curl_result(url), url)
+    response = curl_result(url)
+    parse_response(response, url)
   end
 
   def curl_result(url)
@@ -43,21 +42,25 @@ class SmokeTest
     end
   end
 
-  def parse_result(curl_result, url)
-    case curl_result
-    when "200" then "#{url} passed the smoke test\n"
-    when "302" then "#{url} passed the smoke test with a redirection\n"
-    else "#{url} failed the smoke test\n"
+  def parse_response(response, url)
+    case response
+    when "200" then "#{url} passed the smoke test"
+    when "302" then "#{url} passed the smoke test with a redirection"
+    else "#{url} failed the smoke test"
     end
   end
 
-  def slack_smoke_notification(message)
+  def format_messages(messages)
+    "Smoke Test results\n" + messages.join("\n")
+  end
+
+  def slack_smoke_notification(messages)
     uri = URI.parse("#{@slack_uri}/#{@slack_token}")
 
     payload = {
       channel:    @slack_room,
       username:   @slack_username,
-      text:       message,
+      text:       format_messages(messages),
       icon_emoji: @slack_emoji
     }
 
